6c6a7039180918ba5a5dc77816d830c6506940a8
victorhdchagas <victorhdchagas@live.com>
2023-06-06 08:50:12 -0300

    Adicionando Commit

--- news-api/src/routes/news.route.ts
+++ news-api/src/routes/news.route.ts
@@ -6,12 +6,9 @@ const router = Router();
 
 router.get("/",async (req,res,next)=>{
     const params = req.query as unknown as  NewsApiParams;
-    console.log('requesting',params)
     const newsController = new NewsController();
     if(!params.q) return res.json({"message":"nothing found"})
     const toReturn = await newsController.find(params)
-    // if(toReturn.status == "ok")
-    //     return res.send(toReturn.articles.map((article:any)=>({...article, publishedAt: new Date(article.publishedAt)})))
     res.send(toReturn)
 })
 
